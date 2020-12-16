76759d71d40f90762c7e9e169ae3a3ced96ec3f9
swarwerth <reloxgamesfr@gmail.com>
2020-12-16 21:56:21 +0100

    🎋 Bug fixes & update - v.0.2.2

--- events/logs/ready.js
+++ events/logs/ready.js
@@ -2,7 +2,7 @@ const config = require('../../config/bot.json')
 
 module.exports = client => {
 
-  console.log(`------------------------------------\nMerci d'avoir choisi rxwS !\nModules : https://github.com/ZerioDev/Music-bot/\nPrêt sur ${client.guilds.cache.size} serveurs !\n------------------------------------`);!
+  console.log(`------------------------------------\nMerci d'avoir choisi rxwS !\nCode source : https://github.com/Swarwerth/rxwS/\nPrêt sur ${client.guilds.cache.size} serveurs !\n------------------------------------`);!
   client.user.setPresence({activity: {name: config.activityname, type: config.activitytype}, status: config.status});
 
 };
