db3f14b6c291f7977485f9b1510d878fbfc53bb6
swarwerth <reloxgamesfr@gmail.com>
2020-12-27 17:40:35 +0100

    🍕 Update 20w52d
    # Update
    - botinfo : add licence

--- commands/bot/botinfo.js
+++ commands/bot/botinfo.js
@@ -8,6 +8,7 @@ module.exports.run = (client, message, args) => {
     .setAuthor(`🤖 Informations sur le bot`, client.user.displayAvatarURL({dynamic: true, size: 4096, format: 'png'}))
     .setTitle(`Github/Swarwerth/rxwS`)
     .setURL(`https://github.com/Swarwerth/rxwS/`)
+    .setDescription(`Le code de ce bot est **sous licence**. Merci de mentionner le nom de Swarwerth !`)
     .setThumbnail(client.user.displayAvatarURL({dynamic: true, size: 4096, format: 'png'}))
     .addFields(
       {name: `> Tag`, value: `\`${client.user.tag}\``, inline: true},
